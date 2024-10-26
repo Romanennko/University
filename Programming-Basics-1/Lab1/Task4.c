dc3532389db0289605b04769f56cfd33121ff008
Mykyta <69142225+Romanennko@users.noreply.github.com>
2024-10-26 22:48:43 +0200

    Create Task4.c

---
+++ Programming-Basics-1/Lab1/Task4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    short int b;
+    double c;
+
+    printf("The value of the variable a (int): %d\n", a);
+    printf("The value of the variable b (short int): %d\n", b);
+    printf("The value of the variable c (double): %f\n", c);
+
+    return 0;
+}
